f50d51812bf04f352215543fe87d93518c44336a
SRISAIYEEGHARAN <100680347@student.swin.edu.au>
2015-10-16 18:43:56 +1100

    Modified the Registration page
    Registration has been added with php for header, navigation and footer.
    New addition of fields such as address and phone number.

--- register_form.php
+++ register_form.php
@@ -10,43 +10,16 @@
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
 	</head>
-	<body class="landing">
 
-		<!-- Header -->
-			<header id="header" class="alt">
-				<h1><a href="index.html"><span class='assignment'>Assignment</span>Buddy</a></h1>
-				<a href="#nav">Menu</a>
-			</header>
+	<body>
+		<?php require 'header.php'; ?>
 
-			
 		<!-- Nav -->
-			<nav id="nav">
-				<ul class="links">
-					<div class="profile-container">
-						<div class='profile-unit'>
-							<p>Welcome,<br><strong>Isuru</strong>
-						</div>
-						<div class='profile-pic-unit'>
-							<div class='profilepic' style = 'background-image: url(https://avatars3.githubusercontent.com/u/13775137)'></div>
-						</div>
-					</div>
-					<li><a href="profile.html"><img class='menuicon' src="images/icons/home.svg" alt="" /> Home</a></li>
-					<li><a href="profile.html"><img class='menuicon' src="images/icons/profile.svg" alt="" /> Profile</a></li>
-					<li><a href="login.php"><img class='menuicon' src="images/icons/login.svg" alt="" /> Sign In</a></li>
-					<li><a href="logout.php"><img class='menuicon' src="images/icons/logout.svg" alt="" /> Sign out</a></li>
-					<li><a href="register_form.php"><img class='menuicon' src="images/icons/register.svg" alt="" /> Sign up</a></li>
-					<li><a href="index.html#two"><img class='menuicon' src="images/icons/info.svg" alt="" /> About Us</a></li>
-				</ul>
-			</nav>
-			
-			<!-- Banner -->
-			<section id="banner_register">  
-				<i class="icon"><img id='logo' src="images/icon.svg" alt="" /></i>
-				<h2><span class='assignment'>Assignment</span>Buddy</h2>
-				<!--<p>Register With us</p>-->
-			</section>
-
-			<section id="one" class="wrapper style1">
+			<?php require 'navigation.php';?>
+		
+		<!-- Register -->
+		
+		<section id="one" class="wrapper style1">
 				<div class="inner">
 					<form id="register" method="post" action="register_process.php" <!--novalidate = "novalidate"-->
 					<article class="registerfeature">
@@ -75,6 +48,7 @@
 										<label for="priority-normal">Female</label>
 									</div>									
 									</p>
+									<!--
 									<p>
 									<label for="state">University&#42;:</label>
 									<div class="12u$">
@@ -93,17 +67,23 @@
 										</div>
 									</div>
 									</p>
+									-->
+									<p>
+										<label for="pmphone">Mobile Phone Number:</label>
+										<input type="tel" name="pmphone" id="pmphone" placeholder="04########" pattern="[0-9]{10}" maxlength="10" size="12" required="required" />
+									</p>
+									<p>
+										<p><label for="aaddress">Address:</label>
+										<input type="text" name="aaddress" id="aaddress"  required="required" placeholder="Enter Your Address" /></p>
+									</p>
 									<p>
 										<label for="pemail">University Email Address&#42;:</label>
 										<input type="email" name="pemail" id="pemail" placeholder="123456789@student.swin.edu.au" required="required" />
 									</p>
-									<!-- <p>
-										//<label for="pmphone">Mobile Phone Number&#42;:</label>
-										//<input type="tel" name="pmphone" id="pmphone" placeholder="04########" pattern="[0-9]{10}" maxlength="10" size="12" required="required" />
-									</p> -->
+									
 									<ul class="actions">
 										<li class="actions">
-											<li><input type="submit" class="special" value="Register" /></li>
+											<li><input type="submit" class="special" value="Next" /></li>
 											<li><input type="reset" class="alt" value="Reset" /></li>
 										</li>
 									</ul>
@@ -112,30 +92,10 @@
 					</form>
 				</div>
 			</section>
+		
 
-	<!-- Footer -->
-			<footer id="footer">
-				<div class="inner">
-					<ul class="icons">
-						<li><a href="#" class="icon fa-facebook">
-							<span class="label">Facebook</span>
-						</a></li>
-						<li><a href="#" class="icon fa-twitter">
-							<span class="label">Twitter</span>
-						</a></li>
-						<li><a href="#" class="icon fa-instagram">
-							<span class="label">Instagram</span>
-						</a></li>
-						<li><a href="#" class="icon fa-linkedin">
-							<span class="label">LinkedIn</span>
-						</a></li>
-					</ul>
-					<ul class="copyright">
-						<li>2015 &copy; X Fast Games</li>
-						<li>Design by <a href="http://templated.co">templated.co</a>.</li>
-					</ul>
-				</div>
-			</footer>
+		<!-- Footer -->
+			<?php require 'footer.php'; ?>
 
 		<!-- Scripts -->
 			<script src="assets/js/jquery.min.js"></script>
@@ -146,3 +106,5 @@
 
 	</body>
 </html>
+
+
