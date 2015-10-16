97396e09482ad0f76ecaddb952f55e408211fa0a
SRISAIYEEGHARAN <100680347@student.swin.edu.au>
2015-10-16 19:43:12 +1100

    Modified select university
    Modified the template for select university.

--- select_university.php
+++ select_university.php
@@ -1,57 +1,37 @@
 <!DOCTYPE HTML>
-<!--
-	Retrospect by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
 <html>
 	<head>
-		<title>Elements - Retrospect by TEMPLATED</title>
+		<title>Select University</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
 	</head>
-	<body>
 
-		<!-- Header -->
-			<header id="header" class="skel-layers-fixed">
-				<h1><a href="index.html">Assignment buddy</a></h1>
-				<a href="#nav">Menu</a>
-			</header>
+	<body>
+		<?php require 'header.php'; ?>
 
 		<!-- Nav -->
-			<nav id="nav">
-				<ul class="links">
-					<li><a href="index.html">Home</a></li>
-					<li><a href="generic.html">Generic</a></li>
-					<li><a href="elements.html">Elements</a></li>
-				</ul>
-			</nav>
-
-		<!-- Main -->
-			<section id="main" class="wrapper">
-				<div class="container">
-					<header class="major special">
-					  <h2>Select a university</h2>
-					</header>
-
-					<!-- Text -->
-				  <section> </section>
-				    <section> </section>
-
-					<!-- Form -->
-						<section>
-							<h3>Please select your university from the below dropdown menu:</h3>
+			<?php require 'navigation.php';?>
+		
+		<!-- Register -->
+		
+		<section id="one" class="wrapper style1">
+				<div class="inner">
+					<form id="register" method="post" action="register_process.php" <!--novalidate = "novalidate"-->
+					<article class="registerfeature">
+							<div class="content">
+							<h2>Please select your university:</h2>
 							<form method="post" action="#">
 								<div class="row uniform 50%">
 									<div class="6u 12u$(xsmall)"></div>
 									<div class="6u$ 12u$(xsmall)"></div>
 									<div class="12u$">
 										<div class="select-wrapper">
-											<select name="category" id="category">
+											<select name="category" id="category" required="required">
 												<option value="">- Category -</option>
 												<option value="1">Monash University</option>
 												<option value="1">RMIT</option>
@@ -60,54 +40,22 @@
 											</select>
 										</div>
 									</div>
-									<div class="4u 12u$(xsmall)"></div>
-									<div class="12u$"></div>
-									<div class="12u$">
-										<ul class="actions">
-											<li><input type="submit" value="Next" class="special" /></li>
-										</ul>
-									  <ul class="actions">
-										  
-								      <h4>Universilty not available? Click button below to proceed: </h4>
-										  <li><input type="reset" value="Click to enter your University details" /></li>
-									  </ul>
-								  </div>
+									<h1>Can't Find the course you are looking for? <a href="create_university.php">Add</a> University</h1>
+														
+									<ul class="actions">
+										<li class="actions">
+											<li><input type="submit" class="special" value="Next" /></li>
+											<!--<li><input type="reset" class="alt" value="Reset" /></li> -->
+										</li>
+									</ul>
 								</div>
-							</form>
-						</section>
-
-					<!-- Image -->
-						<section>
-							<h3>&nbsp;</h3>
-</section>
-
+					</article>
+					</form>
 				</div>
 			</section>
 
 		<!-- Footer -->
-			<footer id="footer">
-				<div class="inner">
-					<ul class="icons">
-						<li><a href="#" class="icon fa-facebook">
-							<span class="label">Facebook</span>
-						</a></li>
-						<li><a href="#" class="icon fa-twitter">
-							<span class="label">Twitter</span>
-						</a></li>
-						<li><a href="#" class="icon fa-instagram">
-							<span class="label">Instagram</span>
-						</a></li>
-						<li><a href="#" class="icon fa-linkedin">
-							<span class="label">LinkedIn</span>
-						</a></li>
-					</ul>
-					<ul class="copyright">
-						<li>&copy; Untitled.</li>
-						<li>Images: <a href="http://unsplash.com">Unsplash</a>.</li>
-						<li>Design: <a href="http://templated.co">TEMPLATED</a>.</li>
-					</ul>
-				</div>
-			</footer>
+			<?php require 'footer.php'; ?>
 
 		<!-- Scripts -->
 			<script src="assets/js/jquery.min.js"></script>
