262e2364d564355928638066ee6e193bd46ba93a
Shameera <100047971@student.swin.edu.au>
2015-10-16 18:01:15 +1100

    Added create unit page

---
+++ create_unit.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+<!--
+	Retrospect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Elements - Retrospect by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<h1><a href="index.html">Retrospect</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="generic.html">Generic</a></li>
+					<li><a href="elements.html">Elements</a></li>
+				</ul>
+			</nav>
+
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="container">
+					<header class="major special">
+					
+
+						
+						
+						<h2>Create Group</h2>
+						<p></p>
+						<h2>STEP 2:</h2>
+
+					</header>
+
+					<!-- Text -->
+						<section>
+							<h2>Create a unit</h2>
+							
+							<!-- Form -->
+						<section>
+							<h3>Please fill in the blanks</h3>
+							<form method="post" action="#">
+							<p>Enter Unit Name:</p>
+								<div class="row uniform 50%">
+									<div class="6u 12u$(xsmall)">
+										<input type="text" name="name" id="name" value="" placeholder="Unit Name" />
+									</div>
+									<p>Enter Unit Code:</p>
+									<div class="6u$ 12u$(xsmall)">
+										<input type="text" name="code" id="code" value="" placeholder="Unit Code" />
+
+									
+									
+									<div class="12u$">
+										<ul class="actions">
+											<li><input type="submit" value="Send Message" class="special" /></li>
+											<li><input type="reset" value="Reset" /></li>
+										</ul>
+									</div>
+								</div>
+							</form>
+						</section>
+
+					
---
+++ select_unit.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML>
+<!--
+	Retrospect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Elements - Retrospect by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<h1><a href="index.html">Retrospect</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="generic.html">Generic</a></li>
+					<li><a href="elements.html">Elements</a></li>
+				</ul>
+			</nav>
+
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="container">
+					<header class="major special">
+					
+
+						
+						
+						<h2>Create Group</h2>
+						<p></p>
+						<h2>STEP 1:</h2>
+
+					</header>
+
+					<!-- Text -->
+						<section>
+							<h2>Select Units</h2>
+							<h4>Please select the units </h4>
+
+							<hr />
+
+							<div class="12u$">
+										<div class="selectunits">
+											<select name="category" id="category">
+												<option value="">COS10009 Introduction to Programming-</option>
+												<option value="1">COS10011 Creating Web Applications</option>
+												<option value="1">COS10003 Computer and Logic Essentials</option>
+												<option value="1">COS20001 User Centred Design</option>
+												<option value="1">COS20007 Object Oriented Programming</option>
+												option>
+												<option value="1">SWE20001 Development Project 1 – Tools and Practices </option>
+												option>
+												<option value="1">COS10004 Computer Systems</option>
+												option>
+												<option value="1">COS20015 Fundamentals of Data Management</option>
+											</select>
+										</div>
+									</div>
+						</div>
+
+ 				<!-- Buttons -->
+ 				<p></p>
+ 				<p></p>
+						<section>
+							
+							<ul align="right" class="actions">
+								<li><a href="#" class="button special">GO NEXT</a></li>
+							</ul>
+							
+						</section>
+
+
+
+
+
+
+
+
+
+
+						<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook">
+							<span class="label">Facebook</span>
+						</a></li>
+						<li><a href="#" class="icon fa-twitter">
+							<span class="label">Twitter</span>
+						</a></li>
+						<li><a href="#" class="icon fa-instagram">
+							<span class="label">Instagram</span>
+						</a></li>
+						<li><a href="#" class="icon fa-linkedin">
+							<span class="label">LinkedIn</span>
+						</a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled.</li>
+						<li>Images: <a href="http://unsplash.com">Unsplash</a>.</li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a>.</li>
+					</ul>
+				</div>
+			</footer>
