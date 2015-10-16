f47e8871fd2ff00708279e0e03607d74b0e6d325
Jaiminshah09 <100024453@student.swin.edu.au>
2015-10-16 19:22:57 +1100

    Added creation of university page
    Added creation of university page

---
+++ create_university.php
@@ -0,0 +1,116 @@
+<!DOCTYPE HTML>
+<!--
+	Retrospect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Elements - Retrospect by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<h1><a href="index.html">Retrospect</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="generic.html">Generic</a></li>
+					<li><a href="elements.html">Elements</a></li>
+				</ul>
+			</nav>
+
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="container">
+					<header class="major special">
+					  <h2>Create university</h2>
+					</header>
+
+					<!-- Text -->
+				  <section> </section>
+				    <section> </section>
+
+					<!-- Form -->
+						<section>
+							<h3>Please enter the university details below *:							</h3>
+							<form method="post" action="#">
+							  <div class="row uniform 50%">
+                        
+<div class="6u 12u$(xsmall)">
+			<p>
+  <input type="text" name="name" id="name" value="" placeholder="Enter University Name" required="required" />
+			</p>
+			<p>
+			  
+			  <input type="text" name="name" id="name" value="" placeholder="Enter University Location" required="required" />
+			</p>
+			<p>
+			  
+			  <input type="text" name="name" id="name" value="" placeholder="Enter University website" required="required" />
+		    </p>
+</div>
+<div class="12u$">
+		    <ul class="actions">
+											<li><input type="submit" value="Next" class="special" /></li>
+		    </ul>
+									  <ul class="actions">
+									  </ul>
+							    </div>
+							  </div>
+							</form>
+						</section>
+
+					<!-- Image -->
+						<section>
+						  <h3>&nbsp;</h3>
+</section>
+
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook">
+							<span class="label">Facebook</span>
+						</a></li>
+						<li><a href="#" class="icon fa-twitter">
+							<span class="label">Twitter</span>
+						</a></li>
+						<li><a href="#" class="icon fa-instagram">
+							<span class="label">Instagram</span>
+						</a></li>
+						<li><a href="#" class="icon fa-linkedin">
+							<span class="label">LinkedIn</span>
+						</a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled.</li>
+						<li>Images: <a href="http://unsplash.com">Unsplash</a>.</li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a>.</li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
