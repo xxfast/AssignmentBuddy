f8dc85fc6192dda5d389bfe8c23bd76f188e02cc
xxfast <isuru@xfastgames.com>
2015-10-24 00:40:07 +1100

    added php validation and sanitiser
    added php validation and sanitiser

--- register_form.php
+++ register_form.php
@@ -33,16 +33,14 @@
 			<section id="one" class="wrapper style1">
 				<div class="inner">
 					<div class='gstarting'>
-						<h2><?php 
-						$email = $_SESSION["username"];
-						echo $email; 
-						?> Signup for AssignmentBuddy!</h2>
+						<h2>Signup for AssignmentBuddy!</h2>
 					<div>
 					<article class="feature right">
 						<span class="image"><img src="images/pic01.png" alt="" /></span>
 						<div class="content">
 							<fieldset>
 								<legend>Step 01: Personal Information</legend>
+							
 							<form method="post" action="register_process.php" validate='validate'>
 								<div class="row uniform 50%">
 									<div class="6u 12u$(xsmall)">
@@ -55,38 +53,44 @@
 										<input type="email" name="pemail" id="pemail" placeholder="student@university.edu" pattern="/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD" required="required" />
 									</div>
 										<div class="2u 12u$(xsmall)">
-											<input type="text" name="pdate" id="pdob"  placeholder="dd" required="required" />
+											<input type="text" name="pdate" id="pdobd"  placeholder="dd" required="required" />
 										</div>
 										<div class="2u 12u$(xsmall)">
-											<input type="text" name="pmonth" id="pdob"  placeholder="mm" required="required" />
+											<input type="text" name="pmonth" id="pdobm"  placeholder="mm" required="required" />
 										</div>
 										<div class="2u 12u$(xsmall)">
-											<input type="text" name="pyear" id="pdob"  placeholder="yyyy" required="required" />
+											<input type="text" name="pyear" id="pdoby"  placeholder="yyyy" required="required" />
 										</div>
 									<div class="6u 12u$(xsmall)">
-										<input type="radio" id="male" name="gender" style="display:none"/>
+										<input type="radio" id="male" name="pgender" value="male" style="display:none"/>
 										<label for="male">Male</label>
-										<input type="radio" id="female" name="gender"  style="display:none"/>
+										<input type="radio" id="female" name="pgender" value="female" style="display:none"/>
 										<label for="female">Female</label>
 									</div>
 									<div class="12u$">
-									<input type="text" name="pmphone" id="pmphone" placeholder="Phone Number (+00)0000000000" pattern="[\+]\d{2}[\(]\d{2}[\)]\d{10}" maxlength="10" size="12" required="required" />
+									<input type="text" name="pphone" id="pmphone" placeholder="Phone Number (+00)0000000000" pattern="[\+]\d{2}[\(]\d{2}[\)]\d{10}" maxlength="10" size="12" required="required" />
 									</div>
 									<div class="8u 12u$(xsmall)">
-									<input type="text" name="adress" id="adress" placeholder="Address" /></p>
+									<input type="text" name="padress" id="adress" placeholder="Address" /></p>
 									</div>
 									<div class="4u 12u$(xsmall)">
 										<?php include_once 'ISO_SelectCountry.php'; ?>
 									</div>
 									<div class="12u$">
-											<input type="checkbox" id="tos" name="tos" style="display:none">
+											<input type="checkbox" id="tos" name="ptos" style="display:none">
 											<label for="tos">I agree for</label>
 											<a href="#">terms of service</a>
+									<?php
+										$errors = $_GET["errors"];
+								 		echo "<div class='errorlist'><ul>$errors</ul></div>";
+							 		?>
 									</div>
+									
 									<div class="12u$">
 										<input type="submit" class="special" value="Next" />
 										<input type="reset" class="alt" value="Reset" />
 									</div>
+
 								</div>
 							</form>
 							</fieldset>
--- validator.php
+++ validator.php
@@ -30,6 +30,21 @@ class Validator
 		return preg_match("/[a-z A-Z]+/i", $name);
 	}
 
+	public function CheckValueInRange($value, $min, $max)
+	{
+		return (($value>=$min) && ($value<=$max));
+	}
+
+	public function CheckValidDate($value)
+	{
+		return preg_match("/(0[1-9]|1[0-9]|2[0-9]|3[01])-(0[1-9]|1[012])-[0-9]{4}/i", $value);
+	}
+
+	public function CheckValidSex($value)
+	{
+		return ($value=="male" || $value=="female");
+	}
+
 
 }
 
